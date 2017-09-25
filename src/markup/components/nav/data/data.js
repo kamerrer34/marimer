cf73f983ddaf2322331c09031cc8befed70d755e
Денис Цварь <cda34@yandex.ru>
2017-09-25 17:15:23 +0300

    init

---
+++ src/markup/components/nav/data/data.js
@@ -0,0 +1,23 @@
+var data = {nav: {
+    main: {
+        items: [
+            {
+                href: '#about',
+                text: 'О продукте'
+            },
+            {
+                href: '#products',
+                text: 'Продуктовая линейка'
+            },
+            {
+                href: '#posts',
+                text: 'Статьи'
+            },
+            {
+                href: '#map',
+                text: 'Места продаж',
+                sel: true
+            }
+        ]
+    }
+}}
